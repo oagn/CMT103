d5e00632d4e956ea81d4b65b4a0b5255c855a851
Agnethe S. Olsen <agnethe.s.olsen@gmail.com>
2017-10-26 11:26:19 +0100

    Wk4, jack_jill, mkDict function returns a dictionary

--- Lab/Wk4/jack_jill.py
+++ Lab/Wk4/jack_jill.py
@@ -1,1 +1,25 @@
-#jack_jill.py
+# jack_jill1.py
+# This program includes a function that takes two tuples (keys and values) as input.
+# It then creates and returns a dictionary.
+
+def mkDict(keys, values):
+	'''The mkDict function tables two tuples (keys and values) that need to be of the same length and returns
+	a dictionary.'''
+	# Check that the two tuples are of the same length
+	if len(keys) != len(values):
+		return 'ERROR: The two tuples are of different length'
+	else:
+		myDict = {}
+		for i in range(len(keys)):
+			myDict[keys[i]] = values[i] 
+		return myDict
+
+
+
+# Main
+# The two following tuples will be taken as input by the mkDict function
+ids=('a','b', 'c', 'd', 'e', 'f')
+mywords=('Tim', 'Sara', 'flew', 'space', 'pair', 'slippers')
+
+# Print the distionary
+print(mkDict(ids,mywords))
