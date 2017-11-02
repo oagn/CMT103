05c5eb804f8bb9921aa0de00bf9c89113ee28bc5
Agnethe S. Olsen <agnethe.s.olsen@gmail.com>
2017-11-02 11:53:14 +0000

    Wk4, final version

--- Lab/Wk4/jack_jill.py
+++ Lab/Wk4/jack_jill.py
@@ -15,15 +15,34 @@ def mkDict(keys, values):
 		return myDict
 
 
+def newRhyme(rhyme, mdict,keys):
+	''' This function takes a rhyme (string with {key} placeholders) and a dictionary as input, and
+	replaces {key} with the corresponding value from the dictionary. 
+	After this the rhyme is saved to a file in H:\My Documents\CMT103\Lab\Wk4, read and printed.
+	NB: If a file with the name dict[a]_dict[b] already exists, this will be overwritten'''
+	filename = mdict[keys[0]]+"_"+mdict[keys[1]]
+	with open("H:\\My Documents\\CMT103\\Lab\\Wk4\\"+filename+".txt", 'w+') as theFile:
+		for i in range(len(keys)):
+			rhyme = rhyme.replace('{'+keys[i]+'}',mdict[keys[i]])
+		theFile.write(rhyme)
+		theFile.seek(0)
+		print(theFile.read())
+	theFile.close()
+	return 0
+
+
 
 # Main
 # The two following tuples will be taken as input by the mkDict function
 def main():
+	rhyme = "{a} and {b} {c} up the {d} \n to fetch a {e} of {f}"
 	ids=('a','b', 'c', 'd', 'e', 'f')
 	mywords=('Tim', 'Sara', 'flew', 'space', 'pair', 'slippers')
 
 	# Print the distionary
-	print(mkDict(ids,mywords))
+	myDict = mkDict(ids,mywords)
+
+	newRhyme(rhyme,myDict,ids)
 
 if __name__ == "__main__":
 	main()
