c890534a82948cc672b50d1f71cda106fdf3b78b
Agnethe S. Olsen <agnethe.s.olsen@gmail.com>
2017-11-16 11:53:27 +0000

    getNode(index) added to SLinkedList

--- Lab/Wk7/SlinkedList.py
+++ Lab/Wk7/SlinkedList.py
@@ -1,12 +1,20 @@
-import Node
-import DLinkedList
 
+class Node():
+	def __init__(self, _ele=None, _next=None):
+		self.element = _ele
+		self.next = _next
+
+	def __str__(self):
+		return "{}-{}".format(self.element, str(self.next))
 
 class SLinkedList():
 	def __init__(self):
 		self.head = None
 		self.tail = None
 		self.size = 0
+
+	def __str__(self):
+		return "LL: {}".format(str(self.head))
 
 	def addFirst(self, _ele):
 		node = Node(_ele, self.head)
@@ -73,19 +81,41 @@ class SLinkedList():
 	def removeLast(self):
 		return remove(self,self.size-1)
 
-	def add_List_to_LL(listType, alist):
-		if listType == 'SLinkedList':
-			ret = SLinkedList()
-			for _ele in alist:
-				ret.addFirst(_ele)
-			return ret
-		elif listType == 'DLinkedList':
-			ret = DLinkedList()
-			for _ele in alist:
-				ret.addFirst(_ele)
-			return ret
-		else: "Please specify SLinkedList or DLikedList"
+	def getNode(self, ind):
+		if ind > self.size or self.size == 0:
+			return None
+		else:
+			i = 0
+			currentN = self.head
+			while i < ind:
+				currentN = currentN.next
+				i += 1
+			return currentN
+
+def add_List_to_LL(listType, alist):
+	if listType == SLinkedList:
+		ret = SLinkedList()
+		for _ele in alist:
+			ret.addFirst(_ele)
+		return ret
+#	elif listType == 'DLinkedList':
+#		ret = DLinkedList()
+#		for _ele in alist:
+#			ret.addFirst(_ele)
+#		return ret
+	else: print("Please specify SLinkedList or DLikedList")
+
+
+def main():
+
+	alist = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+	sll = add_List_to_LL(SLinkedList, alist)
+	print(sll)
+
+	print("The element of Node at position {} is {}".format(3, sll.getNode(3).element))
 
 
 
+if __name__ == '__main__':
+	main()
 
