453d2d29ed3c1caea1d2f4817221baa3ba37d066
Agnethe S. Olsen <agnethe.s.olsen@gmail.com>
2017-11-23 11:47:31 +0000

    Week 8, task 3, initial commit. Framework for function that merges two sorted lists into one sorted list.

---
+++ Lab/Wk8/merge_sorted_lists.py
@@ -0,0 +1,20 @@
+# merge_sorted_lists.py
+# Week 8, task 3
+
+def merger_sorted_lists(listA, listB):
+	'''Input: two sorted lists, listA and listB, both sorted in ascending order (smallest to largest)'''
+	'''Output: one sortde list, listA, with the elements from listB merged in'''
+
+
+
+
+	return
+
+def main():
+	listA = [2, 5, 8, 10, 11]
+	listB = [1, 3, 4, 6, 7, 8, 9, 11, 12, 13]
+	merge_sorted_lists(listA, listB)
+	print(“The final listA:”, listA)
+
+if __name__ == '__main__':
+	main()
