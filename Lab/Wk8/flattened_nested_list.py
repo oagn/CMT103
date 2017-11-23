e6ea34387fd2e4bb0d90a84aea3b9ee18a14b3aa
Agnethe S. Olsen <agnethe.s.olsen@gmail.com>
2017-11-23 11:34:14 +0000

    Week 8, task 2, initial commit. Framework for function that flattens a nested list.

---
+++ Lab/Wk8/flattened_nested_list.py
@@ -0,0 +1,16 @@
+# flattened_nested_list.py
+# Week 8, task 2
+
+def flattened_nested_list(nested, outList):
+'''Input: Nested list'''
+'''Output: Flattened nested list'''
+	return
+
+def main():
+	alist=[]
+	flatten_nested_list([[2, 3, [4, 5, [6]]], [7, 8, [[9]]], [], 10, [11]], alist)
+	print(alist)
+
+
+if __name__ == '__main__':
+	main()
